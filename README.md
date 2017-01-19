e71dfec79aefa27de769eae520438bdb48615b59
Kai Schnittcher <kai.schnittcher@gmail.com>
2017-01-19 11:52:15 +0100

    Version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # homebridge-ip-symcon
 Bei homebridge-ip-symcon handelt es sich um ein Plugin für die Homebridge von nfarina (https://github.com/nfarina/homebridge) um IP-Symcon (www.symcon.de) mit Siri zu verbinden.
+
+# Konfiguration
+
+config.json Beispiel:
+
+```
+"accessories": [
+  {
+    "accessory": "IP-Symcon",
+    "name": "Flur Temperatur",
+    "SymconHost": "http://IP:Port",
+    "SymconService": "Temperatur",
+    "pollingTime": 60
+  }
+]
+```
+
+Bei SymconHost muss die URL zu dem IPS Webinterface inkl. Port eingetragen werden.
+Bei SymconService wird hinterlegt, um welchen Service es sich handelt.
+Bei pollingTime wird die Zeit in Sekunden angegeben, in der das Gerät abgefragt werden soll, entfernt man den Parameter aus der config.json wird alle 5 Sekunden gepollt.
+Möglichkeiten für den SymconService wären zur Zeit: Temperatur und Switch, weitere folgen.
+
+In IP-Symcon muss ein Hook (https://www.symcon.de/service/dokumentation/modulreferenz/webhook-control/) angelegt werden.
+
+Beispiel zum Abfragen von Werten:
+
+```
+if($_GET["action"] == "get")  {
+  if($_GET["device"] == "TVBuero") {
+    echo GetValue(39504);
+  }
+  if($_GET["device"] == "Flur Temperatur") {
+    echo GetValue(47209);
+  }
+}
+
+if($_GET["action"] == "setOn")  {
+  //Stecker Büro
+  if($_GET["device"] == "TVBuero")  {
+   ZW_SwitchMode(50077, true); //<-- Beispiel zum schalten für einen Z-Wave Switch.
+echo GetValue(39504);   
+	}
+}
+
+if($_GET["action"] == "setOff")  {
+  //Stecker Büro
+  if($_GET["device"] == "TVBuero")   {
+   ZW_SwitchMode(50077, false); //<-- Beispiel zum schalten für einen Z-Wave Switch.
+		echo GetValue(39504);
+		}
+}
+```
---
+++ index.js
@@ -0,0 +1,143 @@
+'use strict';
+let Service, Characteristic;
+var request = require('request');
+
+module.exports = function(homebridge){
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory('homebridge-ip-sycmon', 'IP-Symcon', IPSymcon);
+};
+
+class IPSymcon {
+  constructor (log, config) {
+    this.log = log;
+    //this.debug = config["debug"] || false;
+    this.name = config["name"];
+    this.host = config["SymconHost"];
+    this.SymconService = config["SymconService"];
+    this.username = config["username"] || ""; // wird noch nicht verwendet
+    this.password = config["password"] || ""; // wird noch nicht verwendet
+    this.polling = config["polling"] || false; // wird noch nicht verwendet
+    this.pollingTime = config["pollingTime"] || 5;
+    this.StatusURL = this.host + '/hook/siri?action=get';
+    this.SetURLOn = this.host + '/hook/siri?action=setOn';
+    this.SetURLOff = this.host + '/hook/siri?action=setOff';
+
+    this.log('Device '+ this.name +' initialization succeeded');
+    this.informationService = new Service.AccessoryInformation();
+    this.informationService
+        .setCharacteristic(Characteristic.Manufacturer, "IP-Symcon")
+        .setCharacteristic(Characteristic.Model, this.SymconService)
+        .setCharacteristic(Characteristic.SerialNumber, "");
+    switch (this.SymconService) {
+      case "Temperatur":
+        this.temperatureService = new Service.TemperatureSensor(this.name)
+        this.temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .on('get', this.getTemperature.bind(this))
+        break;
+      case "Switch":
+        this.switchService = new Service.Switch(this.name);
+        this.switchService
+            .getCharacteristic(Characteristic.On)
+            .on('set', this.setSwitchState.bind(this))
+            .on('get', this.getSwitchState.bind(this))
+        break;
+    }
+    setInterval(this.devicePolling.bind(this), this.pollingTime * 1000);
+  }
+
+  httpRequest (url, body, method, username, password, sendimmediately, callback) {
+    request({
+      url: url,
+      body: body,
+      method: method,
+      rejectUnauthorized: false,
+      auth: {
+        user: username,
+        pass: password,
+        sendImmediately: sendimmediately
+      }
+    },
+    function (error, response, body) {
+      callback(error, response, body)
+    })
+  }
+  getTemperature (callback) {
+    this.log('Getting Temperature...');
+    var url = encodeURI(this.StatusURL + '&device='+ this.name);
+    this.httpRequest(url, '', 'GET', '', '', '', function (error, response, responseBody) {
+      if (error) {
+        this.log('HTTP get power function failed: %s', error.message);
+        callback(error);
+      } else {
+        var temperature = parseFloat(responseBody);
+      }
+      this.log('Currently Temperature %s', temperature);
+      callback(null, temperature);
+    }.bind(this));
+  }
+
+  getSwitchState (callback) {
+    this.log('Getting Switch State...');
+    var url = encodeURI(this.StatusURL + '&device='+ this.name);
+    this.httpRequest(url, '', 'GET', '', '', '', function (error, response, responseBody) {
+      if (error) {
+        this.log('getSwitchState function failed: %s', error.message);
+        callback(error);
+      } else {
+        var binaryState = parseInt(responseBody);
+      }
+      var powerOn = binaryState > 0;
+      this.data = powerOn;
+      this.log('Currently State %s', binaryState);
+      callback(null, powerOn);
+    }.bind(this));
+  }
+
+  setSwitchState (value, callback, context) {
+    this.log('Set Switch State...');
+    var url
+    if (this.data == 1) {
+      url = encodeURI(this.SetURLOff + "&device="+ this.name)
+    } else {
+      url = encodeURI(this.SetURLOn + '&device='+ this.name)
+    }
+    this.log(url)
+    this.httpRequest(url, '', 'GET', '', '', '', function (error, response, responseBody) {
+      if (error) {
+        this.log('getSwitchState function failed: %s', error.message)
+        callback(error)
+      } else {
+        var binaryState = parseInt(responseBody)
+        var powerOn = binaryState > 0
+        this.data = powerOn
+        this.log('Currently State %s', binaryState)
+      }
+      callback();
+    }.bind(this))
+  }
+
+  devicePolling () {
+    switch (this.SymconService) {
+      case "Temperatur":
+        this.temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature).getValue();
+        break;
+      case "Switch":
+        this.switchService
+            .getCharacteristic(Characteristic.On)
+            .getValue();
+        break;
+    }
+  }
+
+  getServices () {
+    switch (this.SymconService) {
+      case "Temperatur":
+        return [this.informationService, this.temperatureService]
+      case "Switch":
+        return [this.informationService, this.switchService]
+    }
+  }
+}
